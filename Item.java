578e15d979b360f6d601e585e0862615cb00691e
Josh-Henderson <122053780+Josh-The-Homie@users.noreply.github.com>
2023-08-27 14:52:42 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,13 +1,22 @@
-
+/**
+ * The Item class represents a generic item that can hold a value of any type.
+ * @param <T> The type of value held by the item.
+ */
 public class Item<T>{
+	//private variable e of type T
 private T e;
-
+	
+/**
+     * @return The value held by the item.
+     */
 public T getE() {
 	return e;
-}
-
+}//end getE
+	/**
+     * @param e The value to be set for the item.
+     */
 public void setE(T e) {
 	this.e = e;
-}
+}//end setE
 
-}
+}//end Item
