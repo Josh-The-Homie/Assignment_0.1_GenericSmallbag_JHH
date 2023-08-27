55b35e0fa8634c4ae3bb07242387c46c97b28865
Josh-Henderson <122053780+Josh-The-Homie@users.noreply.github.com>
2023-08-27 14:54:30 -0500

    Update SmallBag.java

--- SmallBag.java
+++ SmallBag.java
@@ -1,13 +1,19 @@
-
+/**
+ * @param <T> The type of items that the bag can hold.
+ */
 public class SmallBag<T>{
 private T item;
-
+/**
+     * @return The item held by the bag.
+     */
 public T getItem() {
 	return item;
-}
-
+}//end getItem
+  /**
+     * @param item The item to be placed in the bag.
+     */
 public void setItem(T item) {
 	this.item = item;
-}
+}//end SetItem
 
-}
+}//end SmallBag
